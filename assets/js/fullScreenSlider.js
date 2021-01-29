2c4b969e481606fd532658024cdca318146ae78c
Kelvin De Los Angeles <kelvin623@gmail.com>
2021-01-29 10:24:10 +0300

    first commit

---
+++ assets/js/fullScreenSlider.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Splide(".full_screen_slider", {
+        type: "fade",
+        autoplay: true,
+        rewind: true,
+        pauseOnHover: false,
+        pagination: false,
+        arrows: false,
+        fixedHeight: "100vh",
+        fixedWidth: "100vw",
+        cover: true,
+        interval: 3000,
+    }).mount();
+});

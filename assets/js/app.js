81124978d6f37a7a93de65484f773c9178f9d1cc
Kelvin De Los Angeles <kelvin623@gmail.com>
2021-02-04 12:22:06 +0300

    All pages but product details available

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const sidebar = document.getElementById("mobile_sidebar");
-    const menuButton = document.getElementById("menu_button");
+    const menuButton = document.querySelectorAll(".menu_button");
 
-    sidebar.addEventListener("click", () => {
-        sidebar.classList.toggle("open");
-        console.log(sidebar);
-    });
-    menuButton.addEventListener("click", () => {
-        sidebar.classList.toggle("open");
-        console.log(sidebar);
+    menuButton.forEach((a) => {
+        a.addEventListener("click", () => {
+            sidebar.classList.toggle("open");
+            console.log(sidebar);
+        });
     });
 });
